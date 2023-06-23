3020f9d01e8bef28567f45aaaa01e2fb9077552f
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-06-23 13:29:20 -0600

    get marketing token integration

---
+++ cartridges/int_marketing_api/cartridge/scripts/marketing/mkt_token.js
@@ -0,0 +1,40 @@
+var ServiceCredential = require('dw/svc/ServiceCredential');
+var LocalServiceRegistry = require('dw/svc/LocalServiceRegistry');
+var Resource = require('dw/web/Resource');
+
+serviceConfig = {
+  mockExec: function () {
+    return [];
+  },
+
+  createRequest: function (service) {
+    const URL = service.configuration.credential.URL + 'v2/token';
+    const clientId = service.configuration.credential.user;    
+    const clientSecret = service.configuration.credential.password;    
+
+    service.URL = URL;
+    service.addHeader("Content-Type", "application/json");
+    service.setRequestMethod("POST");
+    var requestData = {
+      "grant_type": "client_credentials",
+      "client_id": clientId,
+      "client_secret": clientSecret
+  };
+
+    return JSON.stringify(requestData);
+  },
+
+  parseResponse: function (service, response) {
+    var responseObject = {};
+    try {
+      if (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 204) {
+        responseObject = JSON.parse(response.text);
+      }
+    } catch (error) {
+      throw new Error('Service error with Status Code: ' + service.statusCode);
+    }
+    return JSON.parse(response.text);
+  },
+}
+
+module.exports = LocalServiceRegistry.createService('mkt.token', serviceConfig)
